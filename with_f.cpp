e41a0c3cb9b08dba78b6cd9485fc9bc02ed95acf
Voronova Nadezhda <voronovank@yandex.ru>
2015-12-20 23:52:27 +0300

    add generation.h

--- with_f.cpp
+++ with_f.cpp
@@ -5,28 +5,7 @@
 
 #include "structures.h"
 #include "read_and_write.h"
-
-
-//                      ГЕНЕРАЦИЯ МАТРИЦЫ
-
-// результат подстановки точки A в линейную функцию f
-int result_of_substitution(point A, lin_function f, int k);
-
-// вычисление значения одной клетки матрицы
-bool one_cell(point A, pair_of_lin B, int k);
-
-// проверяет на равенство функции пары
-bool is_equal(pair_of_lin B);
-
-// генерирует столбец матрицы
-std::vector<pair_of_lin> gen_col(int k);
-
-// генерирует строку матрицы
-std::vector<point> gen_str(int k);
-
-// генерирует матрицу
-std::vector<std::vector<bool>> gen_matrix(int k);
-
+#include "generation.h"
 
 //                      ГРАДИЕНТНЫЙ АЛГОРИТМ
 
